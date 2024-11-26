e00850925ef4562d637d8d333962d261e4aff3ac
adam brunson <adam.brunson@yahoo.com>
2024-11-25 21:21:47 -0800

    update contributions controller and add show view

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,2 +1,33 @@
 class ContributionsController < ApplicationController
+
+  def show
+    @contribution = Contribution.find(params[:id])
+    @investment = Investment.new
+    @contribution.investment_id = @contribution
+    @contribution.user_id = current_user
+    @contributions = Contribution.all.where(contribution_id: @contribution.id)
+  end
+
+  def new
+    @contribution = Contribution.new
+  end
+
+  def create
+    @contribution = Contribution.new(contribution_params)
+    if @contribution.save
+      redirect_to @contribution
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @contribution = Contribution.find(params[:id])
+  end
+
+  private
+
+  def contribution_params
+    params.require(:contribution).permit(:amount, :investment_id)
+  end
 end
