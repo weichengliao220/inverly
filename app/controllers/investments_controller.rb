2dcc7f922585749a20e7f50db9464520268f56e0
Wei-Cheng Liao <brianliao220@gmail.com>
2024-11-28 10:06:19 +0900

    defined the formulas in the investments controller

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -27,14 +27,14 @@ class InvestmentsController < ApplicationController
     # beginning_value = response.start_value
     # ending_value = response.end_value
     # inception_year = response.inception_year
-    # @average_rate_of_return = average_rate_of_return()
-    # @conservative_rate_of_return = conservative_rate_of_return
-    # @optimistic_rate_of_return = optimistic_rate_of_return
-    # number_of_months_for_table = [36, 60 , 84, 120, 180, 240, 360]
-    # @future_values_for_table = future_value(100, @average_rate_of_return, number_of_months_for_table)
-    # number_of_months_for_graph = [36, 72, 108, 144, 180, 216, 252, 288, 324, 360]
-    # @future_values_for_graph = future_value(100, @average_rate_of_return, number_of_months_for_graph)
-    # raise
+    @average_rate_of_return = average_rate_of_return()
+    @conservative_rate_of_return = conservative_rate_of_return
+    @optimistic_rate_of_return = optimistic_rate_of_return
+    number_of_months_for_table = [36, 60 , 84, 120, 180, 240, 360]
+    @future_values_for_table = future_value(100, @average_rate_of_return, number_of_months_for_table)
+    number_of_months_for_graph = [36, 72, 108, 144, 180, 216, 252, 288, 324, 360]
+    @future_values_for_graph = future_value(100, @average_rate_of_return, number_of_months_for_graph)
+    raise
   end
 
   def average_rate_of_return(beginning_value = rand((100.0)..(150.0)), ending_value = rand((450.0)..(500.0)), inception_year = rand(2000..2010))
