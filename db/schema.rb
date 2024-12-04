8e04e86f7fc6942ea38d13a8fc68878fd39a2759
Roman HaDuong <roman.haduong@gmail.com>
2024-12-04 10:03:57 +0900

    migration add ai_resume to holdings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_03_052823) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_04_010330) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_03_052823) do
     t.bigint "etf_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ai_resume"
     t.index ["etf_id"], name: "index_holdings_on_etf_id"
   end
 
