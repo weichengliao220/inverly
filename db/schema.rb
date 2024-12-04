c7ea9eb7a19d9adbfdf5f5e97caa932e326d240b
Roman HaDuong <roman.haduong@gmail.com>
2024-12-04 10:11:10 +0900

    added last fetched time to holding to know when to update the ai_resume field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_04_010330) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_04_011040) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_04_010330) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ai_resume"
+    t.datetime "last_fetched_at"
     t.index ["etf_id"], name: "index_holdings_on_etf_id"
   end
 
