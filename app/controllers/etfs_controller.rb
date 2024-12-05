8c9ea0d98d95d895882cc12dfcaa27da1e7a32e1
Wei-Cheng Liao <brianliao220@gmail.com>
2024-12-05 14:47:16 +0900

    working on the additional filter tags function on the etfs-index page

--- app/controllers/etfs_controller.rb
+++ app/controllers/etfs_controller.rb
@@ -2,6 +2,7 @@ class EtfsController < ApplicationController
   before_action :fetch_data_for_holdings, only: :show
   def index
     @holdings_filter = ['APPLE INC', 'NVIDIA CORP', 'MICROSOFT CORP', 'AMAZON.COM INC', 'META PLATFORMS INC CLASS A', 'TESLA INC', 'ALPHABET INC CLASS A', 'NETFLIX INC', 'COSTCO WHOLESALE CORP', 'BERKSHIRE HATHAWAY INC CLASS B']
+    @holdings_tag = ['APPLE INC', 'NVIDIA CORP', 'MICROSOFT CORP', 'AMAZON.COM INC', 'TESLA INC']
     if params[:query].present?
       @etfs = Etf.where("name ILIKE ?", "%#{params[:query]}%")
     elsif params[:holdings].present?
@@ -13,6 +14,15 @@ class EtfsController < ApplicationController
       respond_to do |format|
         format.json { render partial: "etfs/etf_list", formats: :html, locals: {etfs: @etfs} }
       end
+    # elsif params[:tags].present?
+    #   @filter = params[:holdings].split(',') if params[:holdings].present?
+    #   @etfs = Etf.joins(:holdings)
+    #   .where(holdings: { description: @filter })
+    #   .group('etfs.id')
+    #   .having('COUNT(DISTINCT holdings.id) = ?', @filter.size)
+    #   respond_to do |format|
+    #     format.json { render partial: "etfs/etf_list", formats: :html, locals: {etfs: @etfs} }
+    #   end
     else
       @etfs = Etf.all
       @etfs = @etfs.where(category: params[:category]) if params[:category].present?
