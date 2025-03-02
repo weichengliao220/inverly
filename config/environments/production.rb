4ab531de68521a77e2094d0e46fb7c17c1e9dbee
Wei-Cheng Liao <brianliao220@gmail.com>
2025-03-02 15:45:37 +0900

    Fix Gemini API key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 require "active_support/core_ext/integer/time"
+require "dotenv/load"
 
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
